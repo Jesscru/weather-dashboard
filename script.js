0ec4e8ee6583e080d39fd83900420deda782de57
jesscru <jessicacrump7@gmail.com>
2020-11-10 00:38:20 -0700

    added second ajax call for 5-day forecast API

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 var apiKey = "63c42ca33fcb2816693df995f648b2aa";
 var city = 'Austin';
 var cityInput = $('#cityInput').val().trim();
+// url for current weather data 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+// url for 5-day forecast
+var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
+// ajax call for the current weather data API
 $.ajax ({
     url: queryURL,
     method: 'GET'
 
 }).then(function(response){
     console.log(response);
-    
+
 })
+
+$.ajax ({
+    url: queryURL2,
+    method: 'GET'
+
+}).then(function(response){
+    console.log(response);
+
+})
+
+
