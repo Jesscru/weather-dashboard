57a9d7332fa6b0ce4638a239e1dc36f8d2da5c5b
jesscru <jessicacrump7@gmail.com>
2020-11-10 01:18:31 -0700

    displayed ajax items for current weather on the page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var searchBtn = $('#searchBtn');
 var apiKey = "63c42ca33fcb2816693df995f648b2aa";
 var city = 'Austin';
 var cityInput = $('#cityInput').val().trim();
@@ -5,6 +6,16 @@ var cityInput = $('#cityInput').val().trim();
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 // url for 5-day forecast
 var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+// searchBtn.on('click', function(event){
+//     event.preventDefault();
+
+//     var listCity = $('<li>');
+//     listCity.text(cityInput);
+//     listCity.append($('.ul'));
+//     console.log(cityInput);
+// })
+
 
 // ajax call for the current weather data API
 $.ajax ({
@@ -14,14 +25,23 @@ $.ajax ({
 }).then(function(response){
     console.log(response);
 
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+    $('.card-title').text(response.name);
+    $('.temp').text('Temperature: ' + tempF.toFixed(2));
+    $('.humidity').text('Humidity: ' + response.main.humidity);
+    $('.wind').text('Wind Speed: ' + response.wind.speed);
+    $('.description').text('Description: ' + response.weather[0].description);
+
 })
 
+// ajax call for the 5-day forecast API
 $.ajax ({
     url: queryURL2,
     method: 'GET'
 
 }).then(function(response){
-    console.log(response);
+    // console.log(response);
 
 })
 
